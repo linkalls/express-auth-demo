d025570a8bdb60143ef0b08da64b25bf8df4f859
poteto <naonyan1135@gmail.com>
2024-07-30 02:59:38 +0900

    とりあえずリファクタ

--- models/user.js
+++ models/user.js
@@ -1,15 +1,22 @@
 const mongoose = require("mongoose")
-const {Schema} = mongoose
+const { Schema } = mongoose
+const bcrypt = require("bcrypt")
 
 const userSchema = new Schema({
   username: {
     type: String,
-    required: [true,"ユーザー名は必要です"]
+    required: [true, "ユーザー名は必要です"],
   },
   password: {
-    type:String,
-    required: [true,"passwordは必須です"]
-  }
+    type: String,
+    required: [true, "passwordは必須です"],
+  },
 })
 
-module.exports = mongoose.model("User",userSchema)
+userSchema.statics.findAndValidate = async function (username, password) {
+  const foundUser = await this.findOne({ username }) //* thisがUserみたいな意味を持つ
+  const isValid = await bcrypt.compare(password, foundUser.password)
+  return isValid ? foundUser : false //* trueならfoundUserがfalseならfalseが返ってくる
+} //* 自分でstaticsの下に設定したメゾットを作れる
+
+module.exports = mongoose.model("User", userSchema)
